14a224bd581524d0322d663bcf56fc7e893b459a
Christopher Baker <me@christopherbaker.net>
2017-11-09 16:18:16 -0600

    Streamline licenses.

--- src/Encoding/SLIP.h
+++ src/Encoding/SLIP.h
@@ -1,33 +1,9 @@
-// =============================================================================
 //
-// Copyright (c) 2010-2016 Christopher Baker <http://christopherbaker.net>
-// 2016 Antoine Villeret added Christopher's ofx/IO/SLIPEncoding.h here
+// Copyright (c) 2010 Christopher Baker <https://christopherbaker.net>
+// Copyright (c) 2016 Antoine Villeret
 //
-// see : https://github.com/bakercp/ofxIO/blob/master/libs/ofxIO/include/ofx/IO/SLIPEncoding.h
+// SPDX-License-Identifier:	MIT
 //
-// Portions:
-//  Copyright (c) 2011, Jacques Fortier. All rights reserved.
-//  https://github.com/jacquesf/COBS-Consistent-Overhead-Byte-Stuffing
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-// =============================================================================
 
 
 #pragma once
